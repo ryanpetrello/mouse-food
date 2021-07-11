fad90755ce3a076178ec941f8ae5fa54af871899
Ryan Petrello <ryan@ryanpetrello.com>
2021-07-10 23:48:37 -0400

    make it async

--- dining.py
+++ dining.py
@@ -1,3 +1,8 @@
+#
+# $ pip install playwright
+# $ playwright install chromium
+#
+import asyncio
 import argparse
 import collections
 import datetime
@@ -6,7 +11,10 @@ import os
 import os.path
 import sys
 import time
+from distutils.util import strtobool
+
 from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
 MAP = collections.OrderedDict([
     ('be-our-guest', '16660079'),
@@ -32,38 +40,72 @@ def login(page, username, password):
     frame.click('button[type=submit]')
     page.wait_for_selector('#search-time-button button')
 
-def search(username, password, date, guests=4):
+def search(username, password, date, guests=4, headless=False):
     print(f"Searching for availability for {guests} on {date}...")
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        browser_kwargs = {'user_agent': AGENT}
-        if os.path.exists(COOKIE_PATH):
-            browser_kwargs['storage_state'] = COOKIE_PATH
-        context = browser.new_context(**browser_kwargs)
-        page = context.new_page()
-
-        if not os.path.exists(COOKIE_PATH):
-            print(f"Logging in...")
+    if not os.path.exists(COOKIE_PATH):
+        print(f"Logging in...")
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=headless)
+            context = browser.new_context(user_agent=AGENT)
+            page = context.new_page()
             login(page, username, password)
+            context.storage_state(path=COOKIE_PATH)
+            browser.close()
 
-        context.storage_state(path=COOKIE_PATH)
+    urls = {}
+    for k, v in MAP.items():
+        for meal in (LUNCH, DINNER):
+            url = API % {'uuid': v, 'guests': guests, 'date': date, 'meal': meal}
+            urls.setdefault(k, []).append(url)
+    fetch(urls, username, password, headless=headless)
 
-        print(f"Fetching schedule data...")
-        for k, v in MAP.items():
-            print('---')
+
+def fetch(urls, username, password, headless=False):
+
+    async def _fetch(restaurant, _urls, headless=False):
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=headless)
+            context = await browser.new_context(user_agent=AGENT, storage_state=COOKIE_PATH)
+            await context.storage_state(path=COOKIE_PATH)
+            page = await context.new_page()
             times = []
-            for meal in (LUNCH, DINNER):
-                url = API % {'uuid': v, 'guests': guests, 'date': date, 'meal': meal}
-                page.goto(url)
-                data = json.loads(page.inner_text('body'))
-                times.extend(data.get('offers', []))
-            if times:
-                print(f'\x1B[32m {k} \x1B[0m')
-                for t in times:
-                    print(f'    - {t["time"]}')
-            else:
-                print(f'\x1B[31m {k} \x1B[0m')
-        browser.close()
+            for u in _urls:
+                await page.goto(u)
+                body = await page.inner_text('body')
+                body = json.loads(body)
+                body['restaurant'] = restaurant
+                times.append(body)
+            return times
+
+    async def fetch_all(urls, loop, headless):
+        return await asyncio.wait([
+            loop.create_task(
+                _fetch(restaurant, url, headless=headless)
+            ) for restaurant, url in urls.items()
+        ])
+
+    loop = asyncio.get_event_loop()
+
+    print(f"Fetching schedule data...")
+    results = loop.run_until_complete(fetch_all(urls, loop, headless))
+    offers = {}
+    for r in results:
+        for task in r:
+            for time in task.result():
+                if 'offers' in time:
+                    offers.setdefault(
+                        time['restaurant'], []
+                    ).extend([o['time'] for o in time['offers']])
+
+    for k in MAP:
+        if offers.get(k):
+            print(f'\x1B[32m {k} \x1B[0m')
+            for t in offers[k]:
+                print(f'    - {t}')
+        else:
+            print(f'\x1B[31m {k} \x1B[0m')
+
+    loop.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -71,7 +113,9 @@ if __name__ == '__main__':
         'date',
         type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
     )
+    parser.add_argument("--headless", type=strtobool, nargs='?',
+                        const=True, default=True)
     args = parser.parse_args()
     username = os.getenv('DISNEY_USERNAME')
     password = os.getenv('DISNEY_PASSWORD')
-    search(username, password, args.date.date())
+    search(username, password, args.date.date(), headless=True if args.headless else False)
