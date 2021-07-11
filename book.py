d5ad9e9c6371a543c11c9cd5325659580d7e81a8
Ryan Petrello <ryan@ryanpetrello.com>
2021-07-11 00:40:22 -0400

    more restaurants

--- book.py
+++ book.py
@@ -19,9 +19,38 @@ from playwright.sync_api import sync_playwright
 from playwright.async_api import async_playwright
 
 MAP = collections.OrderedDict([
-    ('be-our-guest', '16660079'),
-    ('cinderella', '90002464'),
-    ('crystal-palace', '90002660'),
+    ('be-our-guest', {
+        'id': '16660079',
+        'url': 'https://disneyworld.disney.go.com/dining/magic-kingdom/be-our-guest-restaurant/availability-modal',
+    }),
+    ('ohana', {
+        'id': '90002606',
+        'url': 'https://disneyworld.disney.go.com/dining/polynesian-resort/ohana/availability-modal',
+    }),
+    ('cinderella', {
+        'id': '90002464',
+        'url': 'https://disneyworld.disney.go.com/dining/magic-Kingdom/cinderella-royal-table/availability-modal',
+    }),
+    ('crystal-palace', {
+        'id': '90002660',
+        'url': 'https://disneyworld.disney.go.com/dining/magic-kingdom/crystal-palace/availability-modal',
+    }),
+    ("oga's cantina", {
+        'id': '19267226',
+        'url': 'https://disneyworld.disney.go.com/dining/hollywood-studios/ogas-cantina/availability-modal',
+    }),
+    ('chef mickey', {
+        'id': '90001369',
+        'url': 'https://disneyworld.disney.go.com/dining/contemporary-resort/chef-mickeys/availability-modal',
+    }),
+    #('snow-white-storybook', {
+    #    'id': '$CLOSED$',
+    #    'url': 'https://disneyworld.disney.go.com/dining/wilderness-lodge-resort/artist-point/availability-modal',
+    #}),
+    #('akershus', {
+    #    'id': '$CLOSED$',
+    #    'url': 'https://disneyworld.disney.go.com/dining/epcot/akershus-royal-banquet-hall/availability-modal',
+    #}),
 ])
 
 LUNCH = '80000717'
@@ -61,11 +90,11 @@ def search(username, password, date, guests=4, extra_days=0, headless=False):
     urls = {}
     for k, v in MAP.items():
         for meal in (LUNCH, DINNER):
-            url = API % {'uuid': v, 'guests': guests, 'date': date, 'meal': meal}
+            url = API % {'uuid': v['id'], 'guests': guests, 'date': date, 'meal': meal}
             urls.setdefault(k, []).append(url)
             while extra_days > 0:
                 date += datetime.timedelta(days=1)
-                url = API % {'uuid': v, 'guests': guests, 'date': date, 'meal': meal}
+                url = API % {'uuid': v['id'], 'guests': guests, 'date': date, 'meal': meal}
                 urls[k].append(url)
                 extra_days -= 1
     fetch(urls, username, password, headless=headless)
@@ -110,9 +139,10 @@ def fetch(urls, username, password, headless=False):
                         arrow.get(o['dateTime']).date().strftime('%b %d') + ' @ ' + o['time']
                         for o in time['offers']])
 
-    for k in MAP:
+    for k, v in MAP.items():
         if offers.get(k):
-            print(f'\x1B[32m {k} \x1B[0m')
+            url = v['url']
+            print(f'\x1B[32m {k} \x1B[0m {url}')
             for t in offers[k]:
                 print(f'    - {t}')
         else:
